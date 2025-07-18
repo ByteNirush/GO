0c3f59ac317b1abf5ec51c94928fb11be234caed
ByteNirush <nirushstha@gmail.com>
2025-07-18 09:34:01 +0545

    Scaffolding an API Project

---
+++ fem_project/internal/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"net/http"
+	"database/sql"
+
+	"github.com/ByteNirush/fem_project/internal/api" 
+	"github.com/ByteNirush/fem_project/internal/store"
+	
+)
+
+type Application struct {
+	Logger 			*log.Logger
+	WorkoutHandler 	*api.WorkoutHandler
+	DB 				*sql.DB
+}
+
+func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+	
+	app := &Application{
+		Logger : 		logger,
+		WorkoutHandler: workoutHandler,
+		DB: 			pgDB,
+	}
+
+	return  app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
---
+++ fem_project/internal/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	
+	"github.com/ByteNirush/fem_project/internal/app"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux{
+	r := chi.NewRouter()
+	
+	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandlerGetWorkoutByID)
+
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+	return r
+}
